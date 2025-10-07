ff05a708771d58e03433ddbd025800422a500e3a
JoseAcrf <jose.rufo200@gmail.com>
2025-10-07 14:37:21 -0400

    Update __manifest__.py

---
+++ addonsv2/mt/extra-addons/sync_addons_control/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Sync Addons Control',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Botón para sincronizar módulos desde GitHub',
+    'author': 'Jose & Daemon',
+    'depends': ['base'],
+    'data': [
+        'views/sync_control_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
